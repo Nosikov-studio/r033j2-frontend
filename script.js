fa2678371600919d7e092bf7f1bf236ee228ad88
Nos <zvezdazanatih@gmail.com>
2025-05-29 14:47:29 +0700

    3

--- script.js
+++ script.js
@@ -18,7 +18,6 @@ const f =document.getElementById('f')
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
-      // 'Authorization': `Bearer ${token}`
       },
     body: JSON.stringify({
       ...FormDataObject,
